774e536898db297fd72f1337daa8b44ba04bd3f4
redheadedstepchildtech <monidabusiness@proton.me>
2026-04-24 14:55:59 -0700

    Update Dashboard Card
    Update Dashboard Card

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Donation, HelpOffer } from "@/types";
-import Link from "next/link";
 
 // inside the button area in DashboardCard, when not editing:
 {!editing && (
