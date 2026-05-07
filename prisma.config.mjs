76df39c859f2ed5dbde0bd4e614858d3ca0d606a
redheadedstepchildtech <monidabusiness@proton.me>
2026-05-07 09:24:02 -0700

    Prisma

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "@prisma/config";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasources: {
     db: {
       provider: "postgresql",
-      url: { fromEnv: "DATABASE_URL" },
-    },
-  },
+      url: process.env.DATABASE_URL
+    }
+  }
 });
